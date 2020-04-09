69fd28ab3e7f72849f4df4fceb9a15f7d0d932ea
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-09 01:28:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,6 +3,14 @@ class Dog
       @name
     end
 
+    def name=(name)
+      @name = name
+    end
+    
+    def name
+      @name
+    end
+
     def name=(new_name)
       @name = new_name
     end
